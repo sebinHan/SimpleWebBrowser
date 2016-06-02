17a33f22dc12f9339533a73e17bddf0668fd4619
Sebin Han <gkstpqls17@gmail.com>
2016-06-02 20:45:12 +0900

    Event Programming

--- app/src/main/java/kr/hs/emirim/sbb9918/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sbb9918/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.emirim.sbb9918.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butPre = (Button)findViewById(R.id.but_pre);
         web = (WebView)findViewById(R.id.web_view);
         butMove.setOnClickListener(this);
+        butPre.setOnClickListener(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+        web.loadUrl("http://www.naver.com");
+
+
 
     }
 
@@ -28,5 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.but_move : web.loadUrl(editText.getText().toString()); break;
+            case R.id.but_pre : web.goBack();
+
+        }
     }
 }
